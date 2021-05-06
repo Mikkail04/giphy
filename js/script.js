16f3f801790b69d9a89cc2ca351998b55b02f78b
Glitch (mikkail-giphy-project) <none>
2021-05-06 22:11:32 +0000

    🎁📉 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+let custom_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=clE6kEGmVN8ScIYWOuZzUkSqW6aVPrMH";
 
 $(".search-button").click(function(){
+   fetch(custom_api_url)
   
+  .then(function(response) {
+        	return response.json();
+        })
+
+  .then(function(data) {
+        	console.log(data);
+        });  
 
   
 });
